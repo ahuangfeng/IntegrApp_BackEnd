aa4cd6f9a7207aadaba459a07431d7e8ef9a60c1
Arnau Orriols <arnauorriols@protonmail.com>
2018-04-19 12:02:36 +0200

    saveReport to db

--- report/reportDB.js
+++ report/reportDB.js
@@ -6,5 +6,15 @@ var Report = mongoose.model('Report', models.ReportSchema);
 exports.Report = Report;
 
 exports.saveReport = function (reportData) {
-    
+    var report = new Report(reportData);
+    return new Promise((resolve, reject) => {
+        report.save()
+            .then(reportCreated => {
+                resolve(reportCreated);
+            })
+            .catch(err => {
+                console.log("Error savin report"+err.message)
+                reject(err);
+            });
+    });
 }
