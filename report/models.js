f91cde74a624aad376de79b7547f3b7992fd3f71
Arnau Orriols <arnauorriols@protonmail.com>
2018-05-05 19:02:42 +0200

    updated swagger doc report

--- report/models.js
+++ report/models.js
@@ -1,5 +1,48 @@
 var mongoose = require('mongoose');
 
+/**
+* @swagger
+* definitions:
+*   Report:
+*     required:
+*       - description
+*       - userId
+*       - type
+*       - typeId
+*     properties:
+*       id:
+*         type: string
+*       description:
+*         type: string
+*       userId:
+*         type: string
+*       type:
+*         type: string
+*         enum: [user, advert, forum]
+*       createdAt:
+*         type: string
+*       typeId:
+*         type: string
+*/
+
+/**
+* @swagger
+* definitions:
+*   ReportBody:
+*     required:
+*       - description
+*       - type
+*       - typeId
+*     properties:
+*       description:
+*         type: string
+*       type:
+*         type: string
+*         enum: [user, advert, forum]
+*       typeId:
+*         type: string
+*/
+
 var ReportSchema = new mongoose.Schema(
     {
         description: String,
