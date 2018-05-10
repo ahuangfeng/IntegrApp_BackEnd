acbf1eec064bfc74edbbdea1fd2f6f18b4d16f21
Arnau Orriols <arnauorriols@protonmail.com>
2018-05-10 10:18:42 +0200

    added some tests

--- test/report.js
+++ test/report.js
@@ -22,7 +22,7 @@ describe('POST /report', () => {
             .send({
                 "description": "Description report",
                 "type": "user",
-                "typeId": "5ae1943473e4601b81e1187f",
+                "typeId": "5ae1943473e4601b81e1187f"
             })
             .end(function (err, res) {
                 res.should.have.status(200);
@@ -35,4 +35,94 @@ describe('POST /report', () => {
                 done();
             });
     });
+
+    it('it should not create a report without token', function (done) {
+        chai.request(server)
+            .post('/api/report')
+            .set('Accept', 'application/json')
+            .send({
+                "description": "Description report",
+                "type": "user",
+                "typeId": "5ae1943473e4601b81e1187f"
+            })
+            .end(function (err, res) {
+                res.should.have.status(403);
+                res.body.should.be.an('object');
+                res.body.should.have.property("message");
+                res.body.should.have.property("success");
+                done();
+            });
+    });
+
+    it('it should not create a report from invalid data', function (done) {
+        chai.request(server)
+            .post('/api/report')
+            .set('Accept', 'application/json')
+            .set('x-access-token', configTest.token)
+            .send({
+                "description": "Description report",
+                "type": "user"
+            })
+            .end(function (err, res) {
+                res.should.have.status(400);
+                res.should.be.an('object');
+                res.body.should.have.property('message');
+                done();
+            });
+    });
+
+    it('it should not create a report from invalid type [advert, user, forum]', function (done) {
+        chai.request(server)
+            .post('/api/report')
+            .set('Accept', 'application/json')
+            .set('x-access-token', configTest.token)
+            .send({
+                "description": "Description report",
+                "type": "caca",
+                "typeId": "5ae1943473e4601b81e1187f"
+            })
+            .end(function (err, res) {
+                res.should.have.status(400);
+                res.should.be.an('object');
+                res.body.should.have.property('message');
+                done();
+            });
+    });
+
+    it('it should not create a report from invalid typeId', function (done) {
+        chai.request(server)
+            .post('/api/report')
+            .set('Accept', 'application/json')
+            .set('x-access-token', configTest.token)
+            .send({
+                "description": "Description report",
+                "type": "user",
+                "typeId": "5ae1943473e4601b81e118er"
+            })
+            .end(function (err, res) {
+                res.should.have.status(400);
+                res.should.be.an('object');
+                res.body.should.have.property('message');
+                done();
+            });
+    });
+
+    it('it should not create a report from a type=user and typeId from an advert', function (done) {
+        chai.request(server)
+            .post('/api/report')
+            .set('Accept', 'application/json')
+            .set('x-access-token', configTest.token)
+            .send({
+                "description": "Description report",
+                "type": "user",
+                "typeId": "5aedb9ba76b8fb001442cd86"
+            })
+            .end(function (err, res) {
+                res.should.have.status(400);
+                res.should.be.an('object');
+                res.body.should.have.property('message');
+                done();
+            });
+    });
+
 });
